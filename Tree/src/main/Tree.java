db16efd931bd6358aa79b47a7fe66b61d9412c9f
Matt Dwyer <763431@10.9.34.6>
2015-11-23 14:28:53 -0600

    Continued work on mainWindow

--- Tree/src/main/Tree.java
+++ Tree/src/main/Tree.java
@@ -8,10 +8,13 @@ public class Tree {
     private static LoadingWindow load;
     private static DrawWindow draw;
     private static Thread mainWindow;
+    public static boolean running;
+    public static boolean fullScreen = false;
     
     public static void main(String[] args) {
-        load = new LoadingWindow();
-        draw = new DrawWindow();
+        running = true;
+    	load = new LoadingWindow();
+        draw = new DrawWindow(fullScreen);
         mainWindow = new Thread(draw);
         
         mainWindow.start();
@@ -20,8 +23,9 @@ public class Tree {
             try {
                 load.advance("Creating Window");
                 mainWindow.wait();
-                load.advance("Creating Menues");
+                load.advance("Creating Menus");
                 mainWindow.wait();
+                load.advance("Sure");
             }
             catch (Exception e) {
                 e.printStackTrace();
--- Tree/src/windows/DrawWindow.java
+++ Tree/src/windows/DrawWindow.java
@@ -2,41 +2,77 @@ package windows;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
+import main.Tree;
+
 public class DrawWindow implements Runnable {
-    
-    private JFrame frame;
-    private Dimension size;
-    
-    public DrawWindow() {
-        frame = new JFrame();
-        size = Toolkit.getDefaultToolkit().getScreenSize();
-        
-        frame.setVisible(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(size);
-        frame.setResizable(false);
-        frame.setBackground(Color.GREEN);
-        frame.setLocation(0, 0);
-    }
-    
-    @Override
-    public void run() {
-        
-    }
-    
-    protected void buildMenuBar() {
-        
-    }
-    
-    public void resize(Dimension size) {
-        frame.setSize(size);
-    }
-    
-    public void resize(int width, int height) {
-        frame.setSize(width, height);
-    }
+
+	private JFrame frame;
+	private Dimension size;
+	private int backgroundRGB;
+	public int pixels[][];
+
+	public DrawWindow(boolean fullScreen) {
+		frame = new JFrame();
+		size = Toolkit.getDefaultToolkit().getScreenSize();
+		pixels = new int[(int) size.getWidth()][(int) size.getHeight()];
+
+		frame.setVisible(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(size);
+		frame.setResizable(false);
+		frame.setBackground(new Color(backgroundRGB));
+		frame.setLocation(0, 0);
+		
+		if (fullScreen) {
+			frame.setUndecorated(true);
+			frame.setIgnoreRepaint(true);
+			//vc.setFullScreenWindow();
+		}
+	}
+
+	@Override
+	public void run() {
+		notify();
+
+		buildMenuBar();
+		notify();
+
+		synchronized (this) {
+
+			while (Tree.running) {
+				frame.dispose();
+				draw(frame.getGraphics());
+			}
+
+		}
+	}
+
+	private void draw(Graphics g) {
+		for (int x = 0; x <= pixels.length; x++) {
+			for (int y = 0; y <= pixels[x].length; y++) {
+				
+			}
+		}
+	}
+
+	protected void buildMenuBar() {
+
+	}
+
+	protected void setBackgroundColor(int RGB) {
+		frame.setBackground(new Color(RGB));
+	}
+
+	public void resize(Dimension size) {
+		frame.setSize(size);
+	}
+
+	public void resize(int width, int height) {
+		frame.setSize(width, height);
+	}
 }
