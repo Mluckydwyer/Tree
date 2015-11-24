56811a359525788301315bd373cb030e5a4ddafd
Matt Dwyer <763431@10.9.30.172>
2015-11-24 14:36:06 -0600

    Setting up buffer strategy

--- Tree/src/windows/DrawWindow.java
+++ Tree/src/windows/DrawWindow.java
@@ -4,34 +4,37 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 
 import main.Tree;
 
-public class DrawWindow implements Runnable {
+public class DrawWindow extends JFrame implements Runnable {
 
-	private JFrame frame;
 	private Dimension size;
 	private int backgroundRGB;
-	public int pixels[][];
+	private int pixels[];
+	private BufferedImage img;
+	private BufferStrategy bs;
 
 	public DrawWindow(boolean fullScreen) {
-		frame = new JFrame();
 		size = Toolkit.getDefaultToolkit().getScreenSize();
-		pixels = new int[(int) size.getWidth()][(int) size.getHeight()];
 
-		frame.setVisible(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(size);
-		frame.setResizable(false);
-		frame.setBackground(new Color(backgroundRGB));
-		frame.setLocation(0, 0);
-		
+		setVisible(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(size);
+		setResizable(false);
+		setBackground(new Color(backgroundRGB));
+		setLocation(0, 0);
+
 		if (fullScreen) {
-			frame.setUndecorated(true);
-			frame.setIgnoreRepaint(true);
-			//vc.setFullScreenWindow();
+			setUndecorated(true);
+			setIgnoreRepaint(true);
+			// vc.setFullScreenWindow();
 		}
 	}
 
@@ -39,40 +42,61 @@ public class DrawWindow implements Runnable {
 	public void run() {
 		notify();
 
-		buildMenuBar();
+		add(buildMenuBar());
 		notify();
 
 		synchronized (this) {
+			img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+			pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
+			BufferStrategy bs = getBufferStrategy();
 
-			while (Tree.running) {
-				frame.dispose();
-				draw(frame.getGraphics());
+			if (bs == null)
+				createBufferStrategy(3);
+
+			for (int i = 0; i < WIDTH * HEIGHT; i++)
+				pixels[i] = 0;
+
+			Graphics g = bs.getDrawGraphics();
+
+			g.drawImage(img, 0, 0, WIDTH, HEIGHT, null);
+			g.dispose();
+			bs.show();
+
+			do {
+				try {
+					g = bs.getDrawGraphics();
+					draw();
+				}
+				finally {
+					g.dispose();
+				}
+				bs.show();
 			}
-
+			while (bs.contentsLost());
 		}
 	}
 
-	private void draw(Graphics g) {
+	private void draw(Graphics g, int pixels[][]) {
 		for (int x = 0; x <= pixels.length; x++) {
 			for (int y = 0; y <= pixels[x].length; y++) {
-				
+
 			}
 		}
 	}
 
-	protected void buildMenuBar() {
-
+	protected JMenuBar buildMenuBar() {
+		return null;
 	}
 
 	protected void setBackgroundColor(int RGB) {
-		frame.setBackground(new Color(RGB));
+		setBackground(new Color(RGB));
 	}
 
 	public void resize(Dimension size) {
-		frame.setSize(size);
+		setSize(size);
 	}
 
 	public void resize(int width, int height) {
-		frame.setSize(width, height);
+		setSize(width, height);
 	}
 }
